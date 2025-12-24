e12f1346add1aed16ea656349f10e128a9cf3aef
TrickyNoodle <sagarshinde2k20@gmail.com>
2025-12-24 15:10:37 +0530

    Login Page

--- app/(User)/login/page.tsx
+++ app/(User)/login/page.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { toast, Toaster } from 'sonner'
+import { useRouter } from 'next/navigation'
 
 const page = () => {
+  const router=useRouter()
+  async function Login(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const loading = toast.loading('Trying to Log In ')
+    const formdata = await new FormData(e.currentTarget)
+    const msg = await fetch('/api/auth/login', {
+      method: 'POST',
+      body: formdata
+    })
+    msg.json().then((data)=>{
+      toast.dismiss(loading)
+      if(data.error){
+        toast.error(data.error)
+      }
+      else{
+        toast.success('Login Succesfull')
+        toast.loading('Redirecting')
+        setTimeout(() => {
+          router.push('/dashboard')
+        }, 3000);
+      }
+    })
+  }
   return (
-    <div>
-        <form action="/api/auth/login" method='POST'>
-            <input type="email" name='email' placeholder='email'/>
-            <input type="password" name='password' placeholder='password' />
-            <button type='submit'>Login</button>
-        </form>
+    <div className='flex flex-col justify-center h-screen items-center gap-4 outline-2'>
+      <Toaster position='top-center' theme='dark' />
+      <h1 className='text-2xl'>Login</h1>
+      <form onSubmit={Login} method='POST' className='flex flex-col gap-2 w-md border-2 p-4 rounded-xl hover:shadow-2xl shadow-green-700 ease-in-out duration-300'>
+        <Input type='email' name='email' placeholder='E-Mail' />
+        <Input type='password' name='password' placeholder='Password' />
+        <Button type='submit' className='w-full'>
+          Login
+        </Button>
+      </form>
     </div>
   )
 }
